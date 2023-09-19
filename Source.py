f6219a01956924115cddab3e17e32d30c4f2b902
John Diaz <104036407+eljonjairo@users.noreply.github.com>
2023-09-19 15:20:41 -0500

    Mw adjusted

--- Source.py
+++ Source.py
@@ -5,17 +5,97 @@
 # Source class
 # 
 
+import numpy as np
+import statsmodels.api as sm
+from scipy import integrate
+from math import log10
+from scipy.interpolate import splev, splrep
+
 class Source:
-    def __init__(self, name, asp_size, kc, Mw ):
-        self.name = name
+    def __init__(self, Fault, Mw, asp_size, kc, rake):
+        self.name = Fault.name
+        self.dhF = Fault.dhF 
+        self.ndip = Fault.ndip
+        self.nstk = Fault.nstk
+        self.target_Mw = Mw
         self.asp_size = asp_size
         self.kc = kc
-        self.Mw = Mw
+        self.rake = rake
+        self.slip = Fault.SlipMat
+        self.stk = Fault.stkVec*1000
+        self.dip = Fault.dipVec*1000
+        self.XF3D = Fault.XF3D
+        self.YF3D = Fault.YF3D
+        self.ZF3D = Fault.ZF3D
 
     def __str__(self):
-        source = " Source's name: " + str(self.name) + "\n"
-        source += " asp_size (km): " + str(self.asp_size)  + "\n"
-        source += " kc: " + str(self.asp_size) + " Mw: " + str(self.Mw) + "\n"
+        source = " Fault info \n"
+        source += " name: " + str(self.name) + "\n"
+        source += " dhF : " + str(self.dhF) + "\n"
+        source += " ndip : " + str(self.ndip) + "\n"
+        source += " nstk : " + str(self.nstk) + "\n \n"
+        source += " Source info \n"
+        source += " asp_size : " + str(self.asp_size) + "\n"
+        source += " kc : " + str(self.kc) + "\n"
+        source += " Target Mw: " + str(self.target_Mw) + "\n"
+        return source
+    
+    def effective_size(self):
+        # Cumulative slip in strike and dip direction
+        sum_slip_stk = np.sum(self.slip, axis=0)
+        sum_slip_dip = np.sum(self.slip, axis=1)
+        
+        acorr_stk = sm.tsa.acf(sum_slip_stk, nlags = len(sum_slip_stk)-1)
+        acorr_dip = sm.tsa.acf(sum_slip_dip, nlags = len(sum_slip_dip)-1)
+        
+        Wacf_stk = integrate.simpson(acorr_stk, self.stk)/max(acorr_stk)
+        Wacf_dip = integrate.simpson(acorr_dip, self.dip)/max(acorr_dip)
+        
+        pass
+        
+    
+    def set_mu_1d(self, vs, rho, h):
+        z = np.zeros(h.size)
+        z[0] = 0
+        for ih in range(0,h.size-1):
+            z[ih+1] = z[ih]-h[ih]          
+       
+        self.mu = np.zeros(self.ZF3D.size)
+        
+        for iz in range(self.ZF3D.size):
+            #print(iz)
+            for ih in range(0,z.size-1):
+                if ( self.ZF3D[iz] <= z[ih] and self.ZF3D[iz] > z[ih+1]):
+                    self.mu[iz] = vs[ih]*vs[ih]*rho[ih]*1e6
+     
+        
+    def adjust_Mw(self):
+                 
+        M0_target = pow(10,(self.target_Mw+10.75)/1.5)
+   
+        moment = 0
+        slip =self.slip.flatten(order='F')
+        area =pow(self.dhF*1000,2)
+        
+        for iz in range(self.mu.size):
+           moment += area*slip[iz]*self.mu[iz]
+            
+        moment = moment*1e-7   # from N-m to dyn-cm    
+        self.original_Mw = -10.75 + 1.5*log10(moment)
+        print(f" Original Mw: %5.2f" % (self.original_Mw))
+        self.adjust_slip = self.slip*M0_target/moment
+        slip =self.adjust_slip.flatten(order='F')
+        for iz in range(self.mu.size):
+           moment += area*slip[iz]*self.mu[iz]
+            
+        moment = moment*1e-7   # from N-m to dyn-cm   
+        self.adjust_Mw = -10.75 + 1.5*log10(moment)
+        
+        print(f" Adjusted Mw: %5.2f" % (self.adjust_Mw))
+        
+        
+        
+        pass
 
 
 
--- SourceGen.py
+++ SourceGen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Generate sliprates and fault coordinates files for DG
+# Generate sliprate files for DG
 #
 # John Diaz January 2023
 #
@@ -15,27 +15,26 @@ import warnings
 
 import numpy as np
 import pickle
-import matplotlib.pyplot as plt
+
 from scipy import signal
 from scipy.io import FortranFile
-from pathlib import Path
 
+import Source
+import plotly.io as io
 
-warnings.filterwarnings("ignore")
-plt.close('all')
-os.system('clear')
+io.renderers.default='svg'
 
-# Name of the Fault file
-FName = "../Outputs/3DFaults/LAquilaCirella03_dhF500m.pickle"
-
-# Name Output files
-name = "LAquilaCirella03_dhF"  # Output files name
+# Load Fault Object
+inFault = '../Outputs/3DFaults/LAquilaCirella03_dhF500m.pickle'
 
 # Output files directory
-outDir = Path('Outputs/Source/')
+outDir = "Outputs/Source/"
+
+# Output files directory
+outDir = "Outputs/Source/"
 
 # ID of simulation
-ID = 3
+ID = 0
 
 # Source Inputs
 tmax = 10.0;
@@ -45,114 +44,144 @@ lowcut  = 1.0                              # srate low pass cut frecuency
 highcut = 0.01                             # srate cut high pass frecuency
 fn = 1/dt/2                                # Nyquist frecuency (Sample rate/2)
 
+# Magnitud Moment
+Mw = 6.0
+
 # Rake angle Cirella 2012
 rake = -90
 
-print("  ")
-print(" START PROGRAM ")
-print("  ")
+# Number of broadband escenarios
+nscen = 4
+# Characteristic asperity size (Km)
+asp_size = 5.0
+#Corner wave number
+kc = 5.0
+
+if __name__ == '__main__':
+
+    warnings.filterwarnings("ignore")
+ 
+    print()
+    print(" ************************************************ ")
+    print(" *        Starting Generate Source Program      * ")
+    print(" ************************************************ ")
+    print()
 
 
-# Load the Fault Dict
-with open(FName, 'rb') as handle:
-    Fault = pickle.load(handle)
+    # Load the Fault Dict
+    with open(inFault, 'rb') as handle:
+        Fault = pickle.load(handle)
 
-nstk = Fault['nstk']
-ndip = Fault['ndip']
+    LAquilaSource = Source.Source(Fault, Mw, asp_size, kc, rake)
+  
+    print(LAquilaSource)
 
-nflt = nstk*ndip
-SlipMat  = Fault['SlipMat']
-RiseTMat = Fault['RiseTMat']
-RupTMat = Fault['RupTMat']
-Slipmean = np.mean(SlipMat)
+    # Velocity Model H(Km),Vp(Km/s),Vs(Km/s) and Rho(Kg/m3) Bianchi, Ameri*
+    h  =  np.array([ 1.00, 1.00, 3.00, 22.0, 15.0, 99.0 ])
+    vp =  np.array([ 3.16, 4.83, 5.76, 6.51, 7.00, 7.80 ])
+    vs =  np.array([ 1.70, 2.60, 3.10, 3.50, 3.80, 4.20 ])
+    rho = np.array([ 2500, 2840, 2940, 3150, 3260, 3500 ])
 
-stkMat = Fault['stkMat']
-dipMat = Fault['dipMat']
+    LAquilaSource.set_mu_1d(vs, rho, h)
+    LAquilaSource.adjust_Mw()
+    
+    
+# nstk = Fault['nstk']
+# ndip = Fault['ndip']
 
-it_start = np.round(RupTMat/dt)
-it_start = it_start.astype(int)
-it_end   = np.round((RupTMat+RiseTMat)/dt)
-it_end   = it_end.astype(int)
+# nflt = nstk*ndip
+# SlipMat  = Fault['SlipMat']
+# RiseTMat = Fault['RiseTMat']
+# RupTMat = Fault['RupTMat']
+# Slipmean = np.mean(SlipMat)
 
-# Sliprate calculation
-SRate = np.zeros([ndip,nstk,nt])
-SRateFilt = np.zeros([ndip,nstk,nt])
-maxsr = np.zeros([ndip,nstk])
-# Filtered Sliprate
-b, a = signal.butter(4, lowcut/fn)
+# stkMat = Fault['stkMat']
+# dipMat = Fault['dipMat']
 
-for idip in range (0,ndip):
-    for istk in range (0,nstk):
-        iti = it_start[idip,istk]
-        itf = it_end[idip,istk]
-        SRate[idip,istk,iti:itf] = SlipMat[idip,istk]/RiseTMat[idip,istk]
-        SRatetmp = SRate[idip,istk,:]
-        SRateFilt[idip,istk,:] = signal.filtfilt(b, a, SRatetmp)
-        maxsr[idip,istk] = np.amax(SRate[idip,istk,:])
+# it_start = np.round(RupTMat/dt)
+# it_start = it_start.astype(int)
+# it_end   = np.round((RupTMat+RiseTMat)/dt)
+# it_end   = it_end.astype(int)
 
-tfig = np.array([0,1,2,3,4,5,7])
-ntfig = int(tfig.size)
-itfig = np.zeros([ntfig,])
-for it in range (0,ntfig):
-    itfig[it] = int(tfig[it]/dt)
+# # Sliprate calculation
+# SRate = np.zeros([ndip,nstk,nt])
+# SRateFilt = np.zeros([ndip,nstk,nt])
+# maxsr = np.zeros([ndip,nstk])
+# # Filtered Sliprate
+# b, a = signal.butter(4, lowcut/fn)
+
+# for idip in range (0,ndip):
+#     for istk in range (0,nstk):
+#         iti = it_start[idip,istk]
+#         itf = it_end[idip,istk]
+#         SRate[idip,istk,iti:itf] = SlipMat[idip,istk]/RiseTMat[idip,istk]
+#         SRatetmp = SRate[idip,istk,:]
+#         SRateFilt[idip,istk,:] = signal.filtfilt(b, a, SRatetmp)
+#         maxsr[idip,istk] = np.amax(SRate[idip,istk,:])
+
+# tfig = np.array([0,1,2,3,4,5,7])
+# ntfig = int(tfig.size)
+# itfig = np.zeros([ntfig,])
+# for it in range (0,ntfig):
+#     itfig[it] = int(tfig[it]/dt)
 
 
-fig, axes = plt.subplots(nrows=8, ncols=1,sharex='col', sharey='row')
-for i,ax in enumerate(axes.flat):
-    it = int(i/dt)
-    itime = ("%3.1f" %(it*dt) )
-    SRatefig = SRateFilt[:,:,it]
-    im=ax.pcolormesh(stkMat,dipMat,SRatefig,cmap="hot_r",vmin=0,vmax=0.3)
-    ax.text(32,7,itime,fontsize=8,fontweight='bold',color='black')
-fig.colorbar(im, ax=axes.ravel().tolist())
+# fig, axes = plt.subplots(nrows=8, ncols=1,sharex='col', sharey='row')
+# for i,ax in enumerate(axes.flat):
+#     it = int(i/dt)
+#     itime = ("%3.1f" %(it*dt) )
+#     SRatefig = SRateFilt[:,:,it]
+#     im=ax.pcolormesh(stkMat,dipMat,SRatefig,cmap="hot_r",vmin=0,vmax=0.3)
+#     ax.text(32,7,itime,fontsize=8,fontweight='bold',color='black')
+# fig.colorbar(im, ax=axes.ravel().tolist())
 
-# Slip positive in the direction of dip and in the direction of strike
-SRdip = (-SRate.flatten(order='F'))*np.sin(np.deg2rad(rake))
-SRstk = (SRate.flatten(order='F'))*np.cos(np.deg2rad(rake))
-SRdip = np.float32(SRdip)
-SRstk = np.float32(SRstk)
-Nsr = np.arange(0,SRdip.size,1)
+# # Slip positive in the direction of dip and in the direction of strike
+# SRdip = (-SRate.flatten(order='F'))*np.sin(np.deg2rad(rake))
+# SRstk = (SRate.flatten(order='F'))*np.cos(np.deg2rad(rake))
+# SRdip = np.float32(SRdip)
+# SRstk = np.float32(SRstk)
+# Nsr = np.arange(0,SRdip.size,1)
 
-outname = Fault['outname']
+# outname = Fault['outname']
 
-SRdipName = '../Outputs/Source/srate_dip_'+outname+'_ID_'+str(ID)
-SRstkName = '../Outputs/Source/srate_str_'+outname+'_ID_'+str(ID)
+# SRdipName = '../Outputs/Source/srate_dip_'+outname+'_ID_'+str(ID)
+# SRstkName = '../Outputs/Source/srate_str_'+outname+'_ID_'+str(ID)
 
-fsrd = FortranFile(SRdipName, 'w')
-fsrd.write_record(SRdip)
-fsrs = FortranFile(SRstkName, 'w')
-fsrs.write_record(SRstk)
+# fsrd = FortranFile(SRdipName, 'w')
+# fsrd.write_record(SRdip)
+# fsrs = FortranFile(SRstkName, 'w')
+# fsrs.write_record(SRstk)
 
-# Write fcoor file
-fcoorHeader = "%d  %d %4.2f " %(nflt, nt, dt)
-fcoor = np.array(Fault['fcoor'])*1000
-fcoorName = '../Outputs/3DFaults/fcoor_'+outname+'_ID_'+str(ID)+'.in'
+# # Write fcoor file
+# fcoorHeader = "%d  %d %4.2f " %(nflt, nt, dt)
+# fcoor = np.array(Fault['fcoor'])*1000
+# fcoorName = '../Outputs/3DFaults/fcoor_'+outname+'_ID_'+str(ID)+'.in'
 
-with open(fcoorName,'wb') as f:
-    np.savetxt(f, fcoor, header=fcoorHeader, comments=' ',fmt = '%9.4f')
+# with open(fcoorName,'wb') as f:
+#     np.savetxt(f, fcoor, header=fcoorHeader, comments=' ',fmt = '%9.4f')
 
-print(f" Coordinates saved in file: {fcoorName}" )
-print(f" SlipRate dip saved in file: {SRdipName}" )
-print(f" SlipRate stk saved in file: {SRstkName}" )
+# print(f" Coordinates saved in file: {fcoorName}" )
+# print(f" SlipRate dip saved in file: {SRdipName}" )
+# print(f" SlipRate stk saved in file: {SRstkName}" )
 
-# Write slip file
-slipHeader = "%d %d %d" %(nstk,ndip,nflt)
+# # Write slip file
+# slipHeader = "%d %d %d" %(nstk,ndip,nflt)
 
-slip = SlipMat.flatten(order='F')
-rise = RiseTMat.flatten(order='F')
-rupt = RupTMat.flatten(order='F')
+# slip = SlipMat.flatten(order='F')
+# rise = RiseTMat.flatten(order='F')
+# rupt = RupTMat.flatten(order='F')
 
-total = np.array([slip,rise,rupt]).transpose()
+# total = np.array([slip,rise,rupt]).transpose()
 
-print(slip[0])
-print(rise[0])
-print(rupt[0])
-slipName = '../Outputs/3DFaults/slip_'+outname+'_ID_'+str(ID)+'.in'
+# print(slip[0])
+# print(rise[0])
+# print(rupt[0])
+# slipName = '../Outputs/3DFaults/slip_'+outname+'_ID_'+str(ID)+'.in'
 
-with open(slipName,'wb') as f:
-    np.savetxt(f, total, header=slipHeader, comments=' ',fmt = '%9.4f')
+# with open(slipName,'wb') as f:
+#     np.savetxt(f, total, header=slipHeader, comments=' ',fmt = '%9.4f')
 
-print(f" slip saved in file: {slipName}" )
+# print(f" slip saved in file: {slipName}" )
 
 
 
